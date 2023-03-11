addce53eb78b3840b6eda353248a80d483333367
Andreurodr <andreurl97@gmail.com>
2023-03-11 11:02:11 +0100

    Corrección ejercicios Arrays

--- ej1.js
+++ ej1.js
@@ -2,14 +2,29 @@
 
 let array = [1, 3, 5, 7];
 console.log(array[0]);
-console.log(array[array.length - array.length]);
-console.log(array[array.indexOf(1)]);
-console.log(array[array.lastIndexOf(1)]);
+console.log(array.shift());
+array.unshift(3);
+console.log(arr.reverse()[arr.length - 1]);
+arr.reverse();
+console.log(arr.slice(0, 1)[0]);
+console.log(arr.splice(0, 1)[0]);
+
+// console.log(array[array.length - array.length]);
+// console.log(array[array.indexOf(1)]);
+// console.log(array[array.lastIndexOf(1)]); estos no sirven ya que no sirven para todos los arrays.
+
+
 
 // ¿Cómo acceder al último elemento de un array? Dime 4 opciones
 
 let arrayB = [5, 10, 15, 20];
 console.log(arrayB[3]);
 console.log(arrayB[arrayB.length - 1]);
-console.log(arrayB[arrayB.indexOf(20)]);
-console.log(arrayB[arrayB.lastIndexOf(20)]);
+console.log(arrayB.slice(-1)[0]);
+console.log(arrayB.pop());
+arrayB.push(5);
+console.log(arrayB.splice(arrayB.length - 1, 1)[0]);
+console.log(arrayB.reverse()[0]);
+// console.log(arrayB[arrayB.indexOf(20)]);
+// console.log(arrayB[arrayB.lastIndexOf(20)]);
+// estos no sirven ya que no sirven para todos los arrays.
--- ej2.js
+++ ej2.js
@@ -4,34 +4,36 @@ const puntajes = [
   [4, 9, 3, 7],
   [6, 1, 8, 5],
 ];
-
+let copiaOriginal = puntajes;
 // Crea una copia de la segunda fila del array y asignalo a una variable nueva.
 
-let puntajesB = puntajes.slice(1, 2);
+//let puntajesB = puntajes.slice(1, 2)[0]; //devuelve un array bidimensional(entonces con el [0] accede dentro de ese array y saca la copia)
+//o simplemente 
+
+let copiaPuntajes = puntajes[1];
 
 // Elimina la segunda fila del array original.
 
-// puntajes.splice(1,1);
+puntajes.splice(1,1);
 
 // Elimina el último elemento de la primera fila del array original.
 
-// puntajes[0].pop();
-
+puntajes[0].pop();
 
 // Agrega el numero 50 al final de la tercera fila del array original.
 
-// puntajes[2].push(50);
+puntajes[1].push(50);
 
 // Eliminar el primer elemento de la tercera fila del array original.
 
-// puntajes[2].shift();
+puntajes[1].shift();
 
 // Agrega el numero 100 al principio de la primera fila del array original.
 
-// puntajes[0].unshift(100);
+puntajes[0].unshift(100);
 
 // Muestra por consola el array original y la copia de la segunda fila creada en el paso 2 para verificar los cambios.
 console.log(puntajes);
-console.log(puntajesB);
+console.log(copiaPuntajes);
 
 //He tenido que comentar los pasos ya que no me dejaba mostrar el array original con los cambios en la consola. Era incompatible quitar la segunda fila(entonces quedan 2 filas restantes) y en el siguiente paso añadir números en la tercera fila(no existente después de quitar la segunda). La consola no leía bien el push(). Mostrando paso por paso en la consola, sí se muestran correctamente los cambios.
